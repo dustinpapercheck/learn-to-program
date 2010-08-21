2a0212991b69991bac0972b947383fb79c953a6b
Dustin Shafae <dustin@PCKMobile6.(none)>
2010-08-20 19:35:33 -0700

    this is a solution that works.

--- orange_tree_complete/orange_tree.rb
+++ orange_tree_complete/orange_tree.rb
@@ -1,33 +1,44 @@
 class OrangeTree < ActiveRecord::Base
 
+
+
   def one_year_passes
-    return "the tree is dead" if not alive?
-    self.age = self.age + 1
-    self.height = self.height + 1 unless mature?
+    self.age = age + 1
+    self.height = height + 1 unless mature?
+    self.fruit = 0
     fruit_increase
   end
-  
+
+  def fruit_increase
+    self.fruit = fruit + 5 unless age < 5
+  end
+
   def mature?
-    self.age > 10
+    age > 10
   end
-  
+
   def alive?
-    self.age <= 30
+    age <= 30
   end
-  
+
   def pick_orange
-    return "the tree is dead" if not alive?
+    return "the%20tree%20is%20dead" if not alive?
     if self.fruit != 0
      self.fruit -= 1
-      taste = ["Mmmmm...that was delicious", "Tastes like orange", "Pucka...yuck.  There is an ant in my orange"]
+      taste = ["Mmmmm...that%20was%20delicious", "Tastes%20like%20orange", "Pucka...yuck.%20There%20is%20an%20ant%20in%20my%20orange"]
       taste[rand(3)]
     else
-      "No fruit"
+      "No%20fruit"
     end
   end
   
+
+
+
   private
   def fruit_increase
-    self.fruit = (self.age - 4) * 5 unless self.age < 5
+    self.fruit = (age - 4) * 5 unless age < 5
   end
+
+
 end
